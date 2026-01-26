4e8dcd89db21e51c43a525f05043584dbbb746bf
cvc953 <cvcorporation05@gmail.com>
2026-01-26 15:44:21 -0500

    fix: Simplify compileSdk configuration in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,9 +5,7 @@ plugins {
 
 android {
     namespace = "com.cvc953.localplayer"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.cvc953.localplayer"
