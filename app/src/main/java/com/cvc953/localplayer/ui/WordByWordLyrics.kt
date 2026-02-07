ac15a999241fb7b1c22cdcec2304fc7a5c4c3074
cvc953 <cvcorporation05@gmail.com>
2026-02-07 14:54:28 -0500

    feat: agregar importación de fillMaxWidth en WordByWordLyrics para mejorar el diseño

--- app/src/main/java/com/cvc953/localplayer/ui/WordByWordLyrics.kt
+++ app/src/main/java/com/cvc953/localplayer/ui/WordByWordLyrics.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
