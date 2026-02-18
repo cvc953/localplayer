fe45a930636f986a1b1e7738e386c85c91483171
cvc953 <cvcorporation05@gmail.com>
2026-02-17 21:47:13 -0500

    Refactor UI components to use MaterialTheme colors and improve code readability
    - Updated AlbumsScreen and PlayerScreen to utilize MaterialTheme for consistent theming.
    - Introduced new color definitions in Color.kt for better color management.
    - Enhanced AlbumDetailScreen with MaterialTheme colors for text and backgrounds.
    - Improved layout and spacing in various UI components for better user experience.
    - Added light theme support in Theme.kt to accommodate light mode styling.

--- app/src/main/java/com/cvc953/localplayer/MainActivity.kt
+++ app/src/main/java/com/cvc953/localplayer/MainActivity.kt
@@ -1,19 +1,21 @@
 package com.cvc953.localplayer
 
+import LocalPlayerTheme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.cvc953.localplayer.ui.MainMusicScreen
 
 class MainActivity : ComponentActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            // Crear la pantalla principal; el ViewModel se inicializará
-            // dentro del bloque de permisos cuando sea necesario.
-            MainMusicScreen {}
+            LocalPlayerTheme {
+                // Crear la pantalla principal; el ViewModel se inicializará
+                // dentro del bloque de permisos cuando sea necesario.
+                MainMusicScreen {}
+            }
         }
     }
 }
