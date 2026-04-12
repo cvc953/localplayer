01fbeea219060246aa59cdafd95a3d9ce3b7fb76
cvc953 <cvcorporation05@gmail.com>
2026-04-12 14:24:49 -0500

    feat: Update version code and version name to 5 and 1.0.5 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.cvc953.localplayer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
